9916bee56a9d0d5c9d850db52cae2405d9af8013
jayson <sondanghuynh@gmail.com>
2024-02-07 14:19:28 +0700

    v1.0.0

--- script.js
+++ script.js
@@ -4,11 +4,35 @@ const button = document.querySelector('button');
 const main_img = document.querySelector(".main-img")
 
 const buttonOK = document.querySelector('.OK')
+const buttonKOK = document.querySelector('.KOK')
 
 buttonOK.addEventListener('mouseover', ()=>{
-    main_img.src = "./img/meo-den.png"
+    main_img.src = "./img/meo-hoicham.png"
 })
 
 buttonOK.addEventListener('mouseout', ()=>{
     main_img.src = "./img/shy_dog.png"
+})
+
+buttonKOK.addEventListener('mouseover', ()=>{
+    main_img.src = "./img/meo-hoicham.png"
+})
+
+buttonKOK.addEventListener('mouseout', ()=>{
+    main_img.src = "./img/shy_dog.png"
+})
+
+buttonKOK.addEventListener('click', ()=>{
+    main_img.src = "./img/download-removebg-preview.png"
+})
+
+buttonOK.addEventListener('click', ()=>{
+    const div_fun = document.querySelector(".fun")
+    const div_not_fun = document.querySelector(".not-fun")
+    const body = document.querySelector('body')
+    const video = div_not_fun.querySelector('video')
+    video.play()
+    div_fun.setAttribute("style", "display:none");
+    div_not_fun.setAttribute("style", "display: flex;")
+    body.setAttribute("style", "background-color: black")
 })
